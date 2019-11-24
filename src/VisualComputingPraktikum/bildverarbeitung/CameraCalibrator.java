5b88bca0c8dd6b784a0c14ba5e2eef35fe5650e7
Timon Lomberg <timon.lomberg@stud.hshl.de>
2019-11-24 16:21:39 +0100

    Added chessboard corner recognition to CameraCalibrator. Awaiting tests!

---
+++ src/VisualComputingPraktikum/bildverarbeitung/CameraCalibrator.java
@@ -0,0 +1,69 @@
+package VisualComputingPraktikum.bildverarbeitung;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Size;
+import org.opencv.core.TermCriteria;
+import org.opencv.imgproc.Imgproc;
+
+enum Modes { CAPTURING, DETECTION, CALIBRATED}
+
+public class CameraCalibrator {
+
+    Modes mode = Modes.CAPTURING;
+
+    int option_height;
+    int option_width;
+    Size boardSize;
+    MatOfPoint2f corners;
+
+
+    /*
+    static int[] testInt = {0};
+    public static void main(String[] args) {    }
+     */
+
+    public CameraCalibrator() {
+        option_height = 8;
+        option_width = 8;
+        boardSize = new Size(option_height, option_width);
+    }
+    public CameraCalibrator(int option_sizeX, int option_sizeY) {
+        this.option_height = option_sizeX;
+        this.option_width = option_sizeY;
+        boardSize = new Size(option_sizeX, option_sizeY);
+    }
+    public CameraCalibrator(Size boardSize) {
+        this.boardSize = boardSize;
+        option_height = (int) boardSize.height;
+        option_width = (int) boardSize.width;
+    }
+
+    /**
+     * Finds chessboard corners in given image
+     *
+     * Outputs the found corners into "corners"
+     *
+     * @param image The input image to find the pattern in
+     * @param sizeX The size of the chessboard pattern (X)
+     * @param sizeY The size of the chessboard pattern (Y)
+     * @return If the method found the pattern
+     */
+    public boolean findCornersChessboard(Mat image, int sizeX, int sizeY) {
+        return Calib3d.findChessboardCorners(image, new Size(sizeX,sizeY), corners,
+                Calib3d.CALIB_CB_ADAPTIVE_THRESH | Calib3d.CALIB_CB_FAST_CHECK | Calib3d.CALIB_CB_NORMALIZE_IMAGE);
+    }
+
+    public void detectAndDrawCorners(Mat image, int height, int width) {
+        boolean found = findCornersChessboard(image, height, width);
+        if(found)
+            Imgproc.cornerSubPix(image, (Mat) corners, new Size(11, 11), new Size(-1,-1),
+                    new TermCriteria(TermCriteria.EPS + TermCriteria.MAX_ITER, 30, 0.1));
+        Calib3d.drawChessboardCorners(image, boardSize, corners, found);
+    }
+
+
+
+
+}
